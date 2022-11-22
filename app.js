4e1c6177faf10a38b3a29afc529c431363b3d95a
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-11-22 16:33:05 -0500

    updated

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
